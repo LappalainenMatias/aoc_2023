14280a62262aa8ecb6eb77215db12dd92c6eb889
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2023-12-01 22:27:03 +0200

    day 1

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:advent_of_code_2023/1_advent_of_code_2023.dart' as advent_of_code_2023;
+
+void main(List<String> arguments) {
+  print('${advent_of_code_2023.calculate()}');
+}
---
+++ test/advent_of_code_2023_test.dart
@@ -0,0 +1,8 @@
+import 'package:advent_of_code_2023/1_advent_of_code_2023.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

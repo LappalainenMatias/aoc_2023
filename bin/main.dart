172e7c007b3c28e3cc786b266d36e6fc5490204e
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2023-12-05 15:23:35 +0200

    add day 5 solution

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,6 @@
 import 'package:advent_of_code_2023/4_aoc_2023.dart';
+import 'package:advent_of_code_2023/5_aoc_2023.dart';
 
 void main(List<String> arguments) {
-  print('${totalPoints()}');
+  print('${minLocation()}');
 }
